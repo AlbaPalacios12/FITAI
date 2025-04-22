ff9df96cb56e1c7097eb8550c87b75eb3c905096
AlbaPalacios12 <albapalacios12@gmail.com>
2025-04-22 21:33:42 +0200

    PantallaBienvenida

--- app/src/main/java/com/example/fitai/MainActivity.kt
+++ app/src/main/java/com/example/fitai/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.fitai
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fitai.ui.theme.FitAITheme
+import com.example.fitai.ui.theme.screens.PantallaBienvenida
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
@@ -20,33 +23,16 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            FitAITheme {
-                Scaffold(modifier = Modifier.fillMaxSize())
-                { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MaterialTheme {
+                PantallaBienvenida {
+                    // Aquí puedes hacer algo cuando se pulse "Comenzar"
+                    Log.d("PantallaInicio", "Botón 'Comenzar' pulsado")
                 }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitAITheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/fitai/ui/theme/screens/PantallaBienvenida.kt
@@ -0,0 +1,38 @@
+package com.example.fitai.ui.theme.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+    fun PantallaBienvenida(onComenzar: () -> Unit) { //para decidir desde el main que va a pasar cuando se pulse el boton
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Bienvenido a FitAI", color = Color(0xFFFF3C00), style = MaterialTheme.typography.titleLarge)
+            Text("Confiamos tu proceso", color = Color.White)
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(onClick = onComenzar, colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF3C00))) {
+                Text("Comenzar", color = Color.White)
+            }
+        }
+    }
+
